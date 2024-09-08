b0e8c6a9ebbde0a0d410647d6770391843a32946
Shah Ehsan <shahehsan2002@gmail.com>
2024-09-08 19:12:34 +0600

    route created

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path:"/cart",
+        element:<Cart/>
+      }
+    ],
+  },
+]);
